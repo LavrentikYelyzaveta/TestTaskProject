c490b5174f6e766a1a210acd4363b54ab11be30f
LavrentikYelyzaveta <75446896+LavrentikYelyzaveta@users.noreply.github.com>
2021-04-02 22:13:41 +0300

    Delete Source directory

--- Source/TestTask/TestTask.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "TestTask.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TestTask, "TestTask" );
- 
